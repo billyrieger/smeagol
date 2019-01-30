8f8555425064a0fef699c3de18033e56068a4fc9
Billy Rieger <wrieger@protonmail.com>
2019-01-30 14:11:53 -0500

    Read step size from command line

--- smeagol-cli/src/main.rs
+++ smeagol-cli/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let mut life = smeagol::Life::from_rle_file(std::env::args().nth(1).unwrap()).unwrap();
     println!("loaded");
     for _ in 0..20 {
-        life.step(35328);
+        life.step(std::env::args().nth(2).and_then(|n_str| n_str.parse().ok()).unwrap_or(1000));
         println!("{}", life.generation());
     }
     // life.save_png("out.png");
