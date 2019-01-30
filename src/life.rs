f9f142a2155bb4c8c5a14e6f40677049f7694efc
Billy Rieger <wrieger@protonmail.com>
2019-01-30 01:15:20 -0500

    Let's make it a binary too

--- src/life.rs
+++ src/life.rs
@@ -1,4 +1,5 @@
 use crate::{Cell, Node, Store};
+use png::HasParameters;
 
 pub struct Life {
     root: Node,
@@ -18,7 +19,47 @@ impl Life {
             generation: 0,
         }
     }
+
+    #[cfg(feature = "import-rle")]
+    pub fn from_rle<P>(path: P) -> Result<Self, crate::rle::RleError>
+    where
+        P: AsRef<std::path::Path>,
+    {
+        let rle = crate::rle::Rle::from_file(path)?;
+        let mut alive_cells = rle.alive_cells().into_iter().map(|(x, y)| (x as i64, y as i64)).collect::<Vec<_>>();
+
+        let mut store = Store::new();
+        let mut root = store.create_empty(3);
+
+        if alive_cells.len() > 0 {
+            let x_min = alive_cells.iter().min_by_key(|&(x, _)| x).unwrap().0;
+            let x_max = alive_cells.iter().max_by_key(|&(x, _)| x).unwrap().0;
+            let y_min = alive_cells.iter().min_by_key(|&(_, y)| y).unwrap().1;
+            let y_max = alive_cells.iter().max_by_key(|&(_, y)| y).unwrap().1;
+
+            while x_min < root.min_coord() || x_max > root.max_coord() || y_min < root.min_coord() || y_max > root.max_coord() {
+                root = root.expand(&mut store);
+            }
+
+            root = root.set_cells_alive(&mut store, &mut alive_cells);
+        }
+
+        Ok(Self {
+            root,
+            store,
+            generation: 0,
+        })
+    }
+
+    pub fn generation(&self) -> u128 {
+        self.generation
+    }
+
+    pub fn population(&self) -> u128 {
+        self.root.population()
+    }
 }
+
 
 /// Methods to get and set individual cells.
 impl Life {
@@ -50,13 +91,89 @@ impl Life {
 
 /// Methods to evolve a Life board according to Life rules.
 impl Life {
+    fn pad(&mut self) {
+        while self.root.level() < 3
+            || self.root.ne(&mut self.store).population()
+                != self
+                    .root
+                    .ne(&mut self.store)
+                    .sw(&mut self.store)
+                    .sw(&mut self.store)
+                    .population()
+            || self.root.nw(&mut self.store).population()
+                != self
+                    .root
+                    .nw(&mut self.store)
+                    .se(&mut self.store)
+                    .se(&mut self.store)
+                    .population()
+            || self.root.se(&mut self.store).population()
+                != self
+                    .root
+                    .se(&mut self.store)
+                    .nw(&mut self.store)
+                    .nw(&mut self.store)
+                    .population()
+            || self.root.sw(&mut self.store).population()
+                != self
+                    .root
+                    .sw(&mut self.store)
+                    .ne(&mut self.store)
+                    .ne(&mut self.store)
+                    .population()
+        {
+            self.root = self.root.expand(&mut self.store);
+        }
+    }
+
     pub fn step_pow_2(&mut self, step_log_2: u8) {
+        self.pad();
         let level_cutoff = step_log_2 + 2;
         while self.root.level() < level_cutoff {
             self.root = self.root.expand(&mut self.store);
         }
         self.root = self.root.step(&mut self.store, level_cutoff);
         self.generation += 1 << step_log_2;
+    }
+}
+
+/// Methods to export a Life board.
+impl Life {
+    #[cfg(feature = "export-png")]
+    pub fn save_png<P>(&self, path: P)
+    where
+        P: AsRef<std::path::Path>,
+    {
+        let file = std::fs::File::create(path).unwrap();
+        let writer = std::io::BufWriter::new(file);
+
+        let alive_cells = self.root.get_alive_cells(&self.store);
+        if alive_cells.len() > 0 {
+            let x_min = alive_cells.iter().map(|(x, _)| x).min().cloned().unwrap();
+            let y_min = alive_cells.iter().map(|(_, y)| y).min().cloned().unwrap();
+            let x_max = alive_cells.iter().map(|(x, _)| x).max().cloned().unwrap();
+            let y_max = alive_cells.iter().map(|(_, y)| y).max().cloned().unwrap();
+            let width = x_max - x_min + 1;
+            let height = y_max - y_min + 1;
+
+            // white rectangle
+            let mut data = vec![255u8; (width * height * 4) as usize];
+
+            for &(x, y) in &alive_cells {
+                let offset_x = x - x_min;
+                let offset_y = y - y_min;
+                let index = (4 * (offset_y * width + offset_x)) as usize;
+                data[index] = 0;
+                data[index + 1] = 0;
+                data[index + 2] = 0;
+            }
+
+            let mut encoder = png::Encoder::new(writer, width as u32, height as u32);
+            encoder.set(png::ColorType::RGBA).set(png::BitDepth::Eight);
+
+            let mut writer = encoder.write_header().unwrap();
+            writer.write_image_data(&data).unwrap();
+        }
     }
 }
 
--- src/node/cells.rs
+++ src/node/cells.rs
@@ -129,11 +129,134 @@ impl Node {
             }
         }
     }
+
+    pub fn get_alive_cells(&self, store: &Store)-> Vec<(i64, i64)> {
+        match self.base {
+            NodeBase::Leaf { alive } => if alive { vec![(0, 0)] } else { vec![] },
+            NodeBase::Interior { .. } => {
+                let mut alive_cells = Vec::with_capacity(self.population as usize);
+
+                if self.population > 0 {
+                    if self.level == 1 {
+                        alive_cells.extend(self.nw(store).get_alive_cells(store).into_iter().map(|(x, y)| (x - 1, y - 1)));
+                        alive_cells.extend(self.ne(store).get_alive_cells(store).into_iter().map(|(x, y)| (x, y - 1)));
+                        alive_cells.extend(self.sw(store).get_alive_cells(store).into_iter().map(|(x, y)| (x - 1, y)));
+                        alive_cells.extend(self.se(store).get_alive_cells(store));
+                    } else {
+                        // quarter side length
+                        let offset = 1 << (self.level - 2);
+                        alive_cells.extend(self.nw(store).get_alive_cells(store).into_iter().map(|(x, y)| (x - offset, y - offset)));
+                        alive_cells.extend(self.ne(store).get_alive_cells(store).into_iter().map(|(x, y)| (x + offset, y - offset)));
+                        alive_cells.extend(self.sw(store).get_alive_cells(store).into_iter().map(|(x, y)| (x - offset, y + offset)));
+                        alive_cells.extend(self.se(store).get_alive_cells(store).into_iter().map(|(x, y)| (x + offset, y + offset)));
+                    }
+                }
+
+                alive_cells
+            }
+        }
+    }
+
+    pub fn set_cells_alive(&self, store: &mut Store, coords: &mut [(i64, i64)]) -> Node {
+        self.set_cells_alive_recursive(store, coords, 0, 0)
+    }
+
+    fn set_cells_alive_recursive(&self, store: &mut Store, coords: &mut [(i64, i64)], offset_x: i64, offset_y: i64) -> Node {
+        if coords.len() == 0 {
+            return *self;
+        }
+
+        for &(x, y) in coords.iter() {
+            assert!(x >= self.min_coord() + offset_x);
+            assert!(y >= self.min_coord() + offset_y);
+            assert!(x <= self.max_coord() + offset_x);
+            assert!(y <= self.max_coord() + offset_y);
+        }
+
+        match self.base {
+            NodeBase::Leaf { .. } =>  {
+                assert!(coords.len() == 1);
+                assert_eq!(coords[0].0 - offset_x, 0);
+                assert_eq!(coords[0].1 - offset_y, 0);
+                store.create_leaf(Cell::Alive)
+            }
+
+            NodeBase::Interior { .. } => {
+                let vert_cutoff = partition_vert(coords, offset_y);
+                let (north, south) = coords.split_at_mut(vert_cutoff);
+
+                let horiz_cutoff = partition_horiz(north, offset_x);
+                let (northwest, northeast) = north.split_at_mut(horiz_cutoff);
+
+                let horiz_cutoff = partition_horiz(south, offset_x);
+                let (southwest, southeast) = south.split_at_mut(horiz_cutoff);
+
+                if self.level == 1 {
+                    let nw = self.nw(store).set_cells_alive_recursive(store, northwest, offset_x - 1, offset_y - 1);
+                    let ne = self.ne(store).set_cells_alive_recursive(store, northeast, offset_x, offset_y - 1);
+                    let sw = self.sw(store).set_cells_alive_recursive(store, southwest, offset_x - 1, offset_y);
+                    let se = self.se(store).set_cells_alive_recursive(store, southeast, offset_x, offset_y);
+
+                    store.create_interior(NodeTemplate { ne, nw, se, sw })
+                } else {
+                    // quarter side length
+                    let offset = 1 << (self.level - 2);
+
+                    let nw = self.nw(store).set_cells_alive_recursive(store, northwest, offset_x - offset, offset_y - offset);
+                    let ne = self.ne(store).set_cells_alive_recursive(store, northeast, offset_x + offset, offset_y - offset);
+                    let sw = self.sw(store).set_cells_alive_recursive(store, southwest, offset_x - offset, offset_y + offset);
+                    let se = self.se(store).set_cells_alive_recursive(store, southeast, offset_x + offset, offset_y + offset);
+
+                    store.create_interior(NodeTemplate { ne, nw, se, sw })
+                }
+            }
+        }
+    }
+}
+
+fn partition_horiz(coords: &mut [(i64, i64)], pivot: i64) -> usize {
+    let mut next_index = 0;
+    for i in 0..coords.len() {
+        if coords[i].0 < pivot {
+            coords.swap(i, next_index);
+            next_index += 1;
+        }
+    }
+    next_index
+}
+
+fn partition_vert(coords: &mut [(i64, i64)], pivot: i64) -> usize {
+    let mut next_index = 0;
+    for i in 0..coords.len() {
+        if coords[i].1 < pivot {
+            coords.swap(i, next_index);
+            next_index += 1;
+        }
+    }
+    next_index
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn set_alive() {
+        let mut coords = vec![(0, 0), (1, 1), (-2, 3), (1, -1), (-4, -5)];
+
+        let mut store = Store::new();
+        let node = store.create_empty(4).set_cells_alive(&mut store, &mut coords);
+
+        for &(x, y) in &coords {
+            assert_eq!(node.get_cell(&store, x, y), Cell::Alive);
+        }
+
+        let mut alive_cells = node.get_alive_cells(&store);
+        assert_eq!(alive_cells.len(), 5);
+        coords.sort();
+        alive_cells.sort();
+        assert_eq!(coords, alive_cells);
+    }
 
     fn get_set_helper(level: u8) {
         let mut store = Store::new();
@@ -145,6 +268,10 @@ mod tests {
 
                 let one_alive = empty.set_cell(&mut store, x, y, Cell::Alive);
                 assert_eq!(one_alive.get_cell(&store, x, y), Cell::Alive);
+
+                let alive_cells = one_alive.get_alive_cells(&store);
+                assert_eq!(alive_cells.len(), 1);
+                assert_eq!(alive_cells[0], (x, y));
 
                 let dead_again = one_alive.set_cell(&mut store, x, y, Cell::Dead);
                 assert_eq!(dead_again.get_cell(&store, x, y), Cell::Dead);
@@ -170,5 +297,29 @@ mod tests {
     #[test]
     fn get_set_level_3() {
         get_set_helper(3)
+    }
+
+    #[test]
+    fn partition() {
+        let mut coords = vec![(1, 0), (-2, 1), (3, -2), (-1, -2), (0, 0), (-1, -1), (5, 5)];
+        let pivot = 0;
+
+        let index = partition_horiz(&mut coords, pivot);
+        assert_eq!(index, 3);
+        for i in 0..index {
+            assert!(coords[i].0 < pivot);
+        }
+        for i in index..coords.len() {
+            assert!(coords[i].0 >= pivot);
+        }
+
+        let index = partition_vert(&mut coords, pivot);
+        assert_eq!(index, 3);
+        for i in 0..index {
+            assert!(coords[i].1 < pivot);
+        }
+        for i in index..coords.len() {
+            assert!(coords[i].1 >= pivot);
+        }
     }
 }
