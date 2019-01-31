b67dcc9b353ce4dab8d3c2a5668cd5f561a6d81b
Billy Rieger <wrieger@protonmail.com>
2019-01-31 15:12:58 -0500

    Update main() to loop forever

--- smeagol-cli/src/main.rs
+++ smeagol-cli/src/main.rs
@@ -1,14 +1,12 @@
 fn main() {
     let mut life = smeagol::Life::from_rle_file(std::env::args().nth(1).unwrap()).unwrap();
     println!("loaded");
-    for _ in 0..20 {
-        life.step(
-            std::env::args()
+    let step = std::env::args()
                 .nth(2)
                 .and_then(|n_str| n_str.parse().ok())
-                .unwrap_or(1000),
-        );
-        println!("{}", life.generation());
+                .unwrap_or(1024);
+    loop {
+        life.step(step);
+        println!("{}\n{}\n", life.generation(), life.population());
     }
-    // life.save_png("out.png");
 }
