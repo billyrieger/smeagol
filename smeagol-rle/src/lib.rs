8adaf7c4b17f1a4ab322e56ee7df3f961fabf6c5
Billy Rieger <wrieger@protonmail.com>
2019-02-20 23:52:27 -0500

    Add LICENSE and notice text

--- smeagol-rle/src/lib.rs
+++ smeagol-rle/src/lib.rs
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 //! Crate for working with run-length encoded (RLE) Life patterns.
 #[macro_use]
 extern crate nom;
