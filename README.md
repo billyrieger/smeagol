8371df7652d2016fddc3531e24d7eb68c16bf8f4
Billy Rieger <wrieger@protonmail.com>
2020-04-20 00:24:01 -0400

    Prepare for merge

---
+++ README.md
@@ -0,0 +1,10 @@
+# smeagol
+
+`smeagol` is a Rust library built to simulate large patterns in cellular
+automata similar to Conway's Game of Life.
+
+## License
+
+`smeagol` is licensed under the Mozilla Public License version 2.0. See the
+[license file](https://github.com/billyrieger/smeagol/blob/master/LICENSE) and
+the [MPL 2.0 FAQ](https://www.mozilla.org/en-US/MPL/2.0/FAQ/) for more details.
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,10 @@
 
 #![feature(const_fn, const_if_match)]
 
+mod counter;
+
+use counter::Counter;
+
 /// Conway's Game of Life.
 pub const LIFE: Rule = Rule::new(&[3], &[2, 3]);
 
@@ -37,50 +41,43 @@ pub const LIFE: Rule = Rule::new(&[3], &[2, 3]);
 ///
 /// ```
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
-pub struct Leaf {
-    cells: u64,
-}
+pub struct Leaf(pub u64);
 
 impl Leaf {
-    /// Creates a `Leaf` from a grid of cells represented by a `u64`.
+    /// Creates a `Leaf` from a grid of cells represented by a `[u8; 8]`.
     ///
     /// # Examples
     ///
     /// ```
     /// # use smeagol::Leaf;
-    /// // . . . . . . . .
-    /// // . . . . . . . .
-    /// // . . . . @ . . .
-    /// // . . . . . @ . .
-    /// // . . . @ @ @ . .
-    /// // . . . . . . . .
-    /// // . . . . . . . .
-    /// // . . . . . . . .
-    /// let glider = Leaf::new(
-    ///     0b_00000000_00000000_00001000_00000100_00011100_00000000_00000000_00000000
-    /// );
+    /// let glider = Leaf::from_array([
+    ///     0b_00000000,
+    ///     0b_00000000,
+    ///     0b_00001000,
+    ///     0b_00000100,
+    ///     0b_00011100,
+    ///     0b_00000000,
+    ///     0b_00000000,
+    ///     0b_00000000,
+    /// ]);
     /// ```
-    pub const fn new(cells: u64) -> Self {
-        Self { cells }
-    }
-
     pub const fn from_array(cells: [u8; 8]) -> Self {
-        Self::new(u64::from_be_bytes(cells))
+        Self(u64::from_be_bytes(cells))
     }
 
-    pub const fn shift(&self, dx: i8, dy: i8) -> Self {
-        let mut result = self.cells;
-        result = if dx < 0 {
-            result << (-dx) as u8
+    const fn shift(&self, dx: i8, dy: i8) -> Self {
+        let mut result = *self;
+        result.0 = if dx < 0 {
+            result.0 << (-dx) as u8
         } else {
-            result >> dx as u8
+            result.0 >> dx as u8
         };
-        result = if dy < 0 {
-            result >> (-dy * 8) as u8
+        result.0 = if dy < 0 {
+            result.0 >> (-dy * 8) as u8
         } else {
-            result << (dy * 8) as u8
+            result.0 << (dy * 8) as u8
         };
-        Self::new(result)
+        result
     }
 
     pub const fn tick(&self, rule: &Rule) -> Self {
@@ -124,18 +121,18 @@ impl Rule {
         let survival = self.survival;
 
         let sums = Counter::new()
-            .add(leaf.shift(0, 1).cells)
-            .add(leaf.shift(0, -1).cells)
-            .add(leaf.shift(1, 0).cells)
-            .add(leaf.shift(-1, 0).cells)
-            .add(leaf.shift(1, 1).cells)
-            .add(leaf.shift(-1, -1).cells)
-            .add(leaf.shift(1, -1).cells)
-            .add(leaf.shift(-1, 1).cells)
+            .add(leaf.shift(0, 1).0)
+            .add(leaf.shift(0, -1).0)
+            .add(leaf.shift(1, 0).0)
+            .add(leaf.shift(-1, 0).0)
+            .add(leaf.shift(1, 1).0)
+            .add(leaf.shift(-1, -1).0)
+            .add(leaf.shift(1, -1).0)
+            .add(leaf.shift(-1, 1).0)
             .finish();
 
-        let alive = leaf.cells;
-        let dead = !leaf.cells;
+        let alive = leaf.0;
+        let dead = !leaf.0;
         let result = u64::MIN
             | sums[0] & (dead & birth[0] | alive & survival[0])
             | sums[1] & (dead & birth[1] | alive & survival[1])
@@ -147,56 +144,9 @@ impl Rule {
             | sums[7] & (dead & birth[7] | alive & survival[7])
             | sums[8] & (dead & birth[8] | alive & survival[8]);
 
-        Leaf::new(result)
+        Leaf(result)
     }
 }
-
-struct Counter {
-    bits: [u64; 4],
-}
-
-impl Counter {
-    /// Returns an empty counter initialized to 0.
-    const fn new() -> Self {
-        Self { bits: [0; 4] }
-    }
-
-    const fn add(self, i: u64) -> Self {
-        let [a, b, c, d] = self.bits;
-
-        let b0 = a ^ i; // sum
-        let q = a & i; // carry
-
-        let b1 = b ^ q; // sum
-        let q = b & q; // carry
-
-        let b2 = c ^ q; // sum
-        let q = c & q; // carry
-
-        let b3 = d | q; // saturating sum
-
-        Self {
-            bits: [b0, b1, b2, b3],
-        }
-    }
-
-    #[rustfmt::skip]
-    const fn finish(self) -> [u64; 9] {
-        let [q0, q1, q2, q3] = self.bits;
-        [
-            !q3 & !q2 & !q1 & !q0, // 0000 = 0
-            !q3 & !q2 & !q1 &  q0, // 0001 = 1
-            !q3 & !q2 &  q1 & !q0, // 0010 = 2
-            !q3 & !q2 &  q1 &  q0, // 0011 = 3
-            !q3 &  q2 & !q1 & !q0, // 0100 = 4
-            !q3 &  q2 & !q1 &  q0, // 0101 = 5
-            !q3 &  q2 &  q1 & !q0, // 0110 = 6
-            !q3 &  q2 &  q1 &  q0, // 0111 = 7
-             q3 & !q2 & !q1 & !q0, // 1000 = 8
-        ]
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -215,30 +165,5 @@ mod tests {
             0b_00000000,
         ]);
         assert_eq!(blinker.tick(&life).tick(&life), blinker);
-    }
-
-    #[test]
-    fn counter_histogram() {
-        let sums = Counter::new()
-            .add(0x___0000000F00000000)
-            .add(0x___000000FFF0000000)
-            .add(0x___00000FFFFF000000)
-            .add(0x___0000FFFFFFF00000)
-            .add(0x___000FFFFFFFFF0000)
-            .add(0x___00FFFFFFFFFFF000)
-            .add(0x___0FFFFFFFFFFFFF00)
-            .add(0x___FFFFFFFFFFFFFFF0)
-            //       |1234567876543210|
-            .finish();
-        //           |1234567876543210|
-        assert_eq!(0x_0000000F00000000, sums[8]);
-        assert_eq!(0x_000000F0F0000000, sums[7]);
-        assert_eq!(0x_00000F000F000000, sums[6]);
-        assert_eq!(0x_0000F00000F00000, sums[5]);
-        assert_eq!(0x_000F0000000F0000, sums[4]);
-        assert_eq!(0x_00F000000000F000, sums[3]);
-        assert_eq!(0x_0F00000000000F00, sums[2]);
-        assert_eq!(0x_F0000000000000F0, sums[1]);
-        assert_eq!(0x_000000000000000F, sums[0]);
     }
 }
